b02d128906c471bf6615b596cbb44dc83e9dee8d
Harry Carter <hc61@aru.ac.uk>
2023-06-02 19:20:35 +0100

    Add project files.

---
+++ wwwroot/js/dropdown.js
@@ -0,0 +1,27 @@
+﻿function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("playerSearch");
+    filter = input.value.toUpperCase();
+    dropdownContent = document.getElementById("dropdown-content");
+    showDropdown(dropdownContent);
+
+    dropdownElements = dropdownContent.getElementsByClassName("dropdown-element");
+    for (i = 0; i < dropdownElements.length; i++) {
+        txtValue = dropdownElements[i].textContent || dropdownElements[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            dropdownElements[i].style.display = "";
+        } else {
+            dropdownElements[i].style.display = "none";
+        }
+    }
+}
+
+function showDropdown(dropdownContent) {
+    var input = document.querySelector('.dropdown input');
+
+    if (input.value.length > 0) {
+        dropdownContent.style.display = 'block';
+    } else {
+        dropdownContent.style.display = 'none';
+    }
+}
---
+++ wwwroot/js/form.js
@@ -0,0 +1,51 @@
+﻿function addNewPlayer(firstName, lastName) {
+    toggleNewPlayerFormVisibility();
+    toggleIntro();
+}
+
+function addExistingPlayer(playerId) {
+    var dropdownContent = document.getElementById("dropdown-content");
+    var search = document.getElementById("playerSearch");
+    var cantSeeName = document.getElementById("cant-see-name");
+    dropdownContent.style.display = 'none';
+    search.classList.toggle("hide");
+    cantSeeName.classList.toggle("hide");
+
+    toggleIntro();
+}
+
+function switchForm() {
+    toggleExistingPlayerFormVisibility();
+    toggleNewPlayerFormVisibility();
+}
+
+function resetForm() {
+    toggleExistingPlayerFormVisibility();
+    toggleIntro();
+    document.getElementById("playerSearch").value = "";
+}
+
+function toggleIntro() {
+    var enterName = document.getElementById("enter-name");
+    var success = document.getElementById("success");
+    var addAnother = document.getElementById("add-another");
+    enterName.classList.toggle('hide');
+    success.classList.toggle('hide');
+    addAnother.classList.toggle('hide');
+}
+
+function toggleExistingPlayerFormVisibility() {
+    var search = document.getElementById("playerSearch");
+    var cantSeeName = document.getElementById("cant-see-name");
+    search.classList.toggle("hide");
+    cantSeeName.classList.toggle("hide");
+}
+
+function toggleNewPlayerFormVisibility() {
+    var newPlayerInputs = document.querySelectorAll('.newPlayerInput');
+    var addButton = document.getElementById("add-button");
+    for (var i = 0; i < newPlayerInputs.length; i++) {
+        newPlayerInputs[i].classList.toggle("showInlineBlock");
+    }
+    addButton.classList.toggle("showInlineBlock");
+}
