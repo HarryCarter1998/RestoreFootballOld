948e34a7ccacfc5da666c985dbe97b4999127623
BuildTools <unconfigured@null.spigotmc.org>
2023-08-22 14:37:09 +0100

    saving changes

--- wwwroot/js/teams.js
+++ wwwroot/js/teams.js
@@ -9,8 +9,25 @@ document.addEventListener("DOMContentLoaded", function () {
         $('#loading').hide();
         $('#teams').show();
     });
+
+    var currentDate = (new Date()).addDays(3);
+    var nextWednesday = new Date();
+    nextWednesday.setDate(currentDate.getDate() + (7 - currentDate.getDay() + 3) % 7 + 1);
+    nextWednesday.setHours(21, 30, 0, 0);
+    if (currentDate < nextWednesday) {
+        var targetButton = document.getElementById('enter-results');
+        targetButton.style.display = 'none';
+        setTimeout(function () {
+            targetButton.style.display = 'inline-block';
+        }, nextWednesday - currentDate);
+    }
 });
 
+Date.prototype.addDays = function (days) {
+    var date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
+}
 
 function cancelSignUp(playerId) {
     if (!confirm("Are you sure you want to remove this player from the teams?")) return;
@@ -30,3 +47,5 @@ function cancelSignUp(playerId) {
         }
     });
 }
+
+
