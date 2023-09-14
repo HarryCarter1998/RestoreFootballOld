17c638cc8741d36abe60c2fa48cb4acbe09c0448
BuildTools <unconfigured@null.spigotmc.org>
2023-09-14 17:50:19 +0100

    Stats initial creation

---
+++ Controllers/StatsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestoreFootball.Data.Services;
+using RestoreFootball.Models;
+using RestoreFootball2.Models;
+using System.Diagnostics;
+
+namespace RestoreFootball.Controllers
+{
+    public class StatsController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IPlayerService _playerService;
+        private readonly IGameweekService _gameweekService;
+
+        public StatsController(ILogger<HomeController> logger, IPlayerService playerService, IGameweekService gameweekService)
+        {
+            _logger = logger;
+            _playerService = playerService;
+            _gameweekService = gameweekService;
+        }
+
+        public async Task<IActionResult> IndexAsync()
+        {
+            return View();
+        }
+    }
+}
